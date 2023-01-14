6aeb7e1a6f5c841293655049c559f38e38421695
LEE DONG HO <endgh43@gmail.com>
2023-01-14 23:54:09 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/FragmentFirst.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FragmentFirst : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        //inflater: XML을 화면에 사용할 준비를 한다. (XML-> View로 만들어 준다!)
+        //container : 프래그먼트에서 사용될 XML의 부모뷰
+        return inflater.inflate(R.layout.first_fragment, container, false)
+    }
+}
